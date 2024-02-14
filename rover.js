0aa82d47590ea1aba39cb1050dd24efd49707f27
Christina Chavez <147195321+chrimcha@users.noreply.github.com>
2024-02-14 17:06:42 -0600

    passed test 7

--- rover.js
+++ rover.js
@@ -1,5 +1,21 @@
+const Command = require("./command");
+const Message = require("./message");
+
 class Rover {
-   // Write code here!
+   constructor(position = 0, mode = 'NORMAL', generatorWatts = 110) {
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+   }
+   
+   receiveMessage(message) {
+      let translatedMessage = {
+         message: Message.name,
+         results: Message.commands
+      }
+     
+      return translatedMessage;
+   }
 }
 
 module.exports = Rover;
